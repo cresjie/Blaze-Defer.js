fef07535c2ef6edeb0c0879baf836792a9b70406
Cres Jie <cresjie@gmail.com>
2014-11-07 11:48:34 +0800

    added

--- ghostDefer.js
+++ ghostDefer.js
@@ -3,7 +3,8 @@
 			onload:[],
 			onfinish:[]	
 		},
-		scripts = [];
+		scripts = [],
+		loadedScripts = {};
 	function addReady(el,callback,par){
 		var fired = false,
 			ready = function(){
@@ -26,10 +27,11 @@
 		}
 			
 	}
-	function addEvents(name,fn){
+	function addEvent(name,fn){
 		if(typeof fn === "function"){
 			events[name].push(fn);
 		}
+		return this;
 	}
 	
 	function triggerEvent(name){
@@ -50,7 +52,7 @@
 		return el;
 	}
 
-	function addScript(p){ // parameter should be an array
+	function addScripts(p){ // parameter should be an array
 		for(var i in p)
 			scripts.push(p[i]);
 		return this;
@@ -60,17 +62,20 @@
 			load(scripts[i]);
 		}
 	}
-	function load(par){ // parameter is a script
-		for(var i in par){
-			if(!par[i].d){ // if script is independent
-				var source = par[i].s || par[i]; // check if script is object and get its source url
+	function load(script){ // parameter is a script
+		for(var i in script){
+			if(!script[i].d){ // if script is independent
+				var source = script[i].s || script[i]; // check if script is object and get its source url
 				var el = createElement(source);
-				addReady(el,function(par_i){
-					if(par[par_i].cb)
-						par[par_i].cb(); // trigger callback function
-					loadChildren(par_i);
+				addReady(el,function(index){
+					if(script[index].cb)
+						script[index].cb(); // trigger callback function
+					loadChildren(index);
 				},i);
 				d.querySelector('head').appendChild(el);
+
+				if(script.name)
+					loadedScripts[name] = true; // register the loaded script by its name
 			}
 		}
 
@@ -78,10 +83,15 @@
 	function run(){
 		triggerEvent('onload'); //trigger event onload before scripts are loaded
 		load(scripts);
-		
+		triggerEvent('onfinish');
 	}
 	
 	//contructor execute
-	addReady(d,run);
+	addReady(d,run); //when document is ready execute function run
+
+	w[fn] =  {
+		addEvent:addEvent,
+		addScripts:addScripts
+	};
 
 })(window,document,'ghostDefer');
